e3f94889c7b5bf4ed877e3ac9bc466f57f755d17
Vadim Cherepanov <xenos26@gmail.com>
2024-02-09 21:27:48 +0700

    feat: add hello.py

--- experiments/hello.py
+++ experiments/hello.py
@@ -1,6 +1,13 @@
-print("Hello, world")
+import datetime
 
-answer = input("What's your name? ")
-print("Hello, " + answer)
-print("Hello,", answer)
-print(f"Hello, {answer}")
+# print("Hello, world")
+#
+# answer = input("What's your name? ")
+# print("Hello, " + answer)
+# print("Hello,", answer)
+# print(f"Hello, {answer}")
+
+date_time = datetime.datetime.now()
+time_now = datetime.datetime.now().time()
+date_now = datetime.date.today()
+print(date_now)
